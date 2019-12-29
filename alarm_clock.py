d532b1cf42fc3e798e7d585bf63e5dcde599d4a5
lusiwangyk <59269829+lusiwangyk@users.noreply.github.com>
2019-12-29 17:26:16 -0500

    Create  alarm_clock.py

---
+++ alarm_clock.py
@@ -0,0 +1,18 @@
+#Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, 
+return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, 
+the alarm should be "7:00" and on the weekend it should be "10:00".
+Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+
+
+
+def alarm_clock(day, vacation):
+    if vacation == True:
+      if day == 1 or day == 2 or day == 3 or day == 4 or day == 5:
+        return "10:00"
+      elif day == 0 or day == 6:
+        return "off"
+    elif vacation == False:
+      if day == 1 or day == 2 or day == 3 or day == 4 or day == 5:
+        return "7:00"
+      elif day == 0 or day == 6:
+        return "10:00"
