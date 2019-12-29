e47fc884fe643593db272a16b190981f13b1e400
lusiwangyk <59269829+lusiwangyk@users.noreply.github.com>
2019-12-29 12:56:08 -0500

    Create sorta_sum.py

---
+++ sorta_sum.py
@@ -0,0 +1,10 @@
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+def sorta_sum(a, b):
+  if a + b <= 9:
+    return a + b
+  elif a + b > 9 and a + b <20:
+    return 20
+  else:
+    return a + b
